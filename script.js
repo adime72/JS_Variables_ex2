7f3eae1881af398350dbf4644d2e3b61723f6730
Adi Meirovitch <adime.support@gmail.com>
2017-10-30 12:21:07 +0200

    creating an object about myself including phase 2 location

---
+++ script.js
@@ -0,0 +1,23 @@
+var me = {};
+console.log(me);
+console.log(typeof me);
+
+// Creating an Object with data
+me = {
+  eyes: "blue",
+  name: "Adi",
+  hight: 176,
+
+  },
+
+console.log(JSON.stringify(me));
+
+// Changing eyes color in object
+me.eyes = "brownish";
+
+console.log(me.eyes);
+me.location= {               // Object inside an object
+    isIsrael: true,
+    city: "Kfar Yona"
+};
+
